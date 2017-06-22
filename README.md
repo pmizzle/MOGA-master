cc850ef0646b0468fe68e0012fb43d2676c10558
paul <p.mizzle9@gmail.com>
2017-06-22 13:35:38 -0500

    Add project files.

---
+++ README.md
@@ -0,0 +1,14 @@
+# MOGA
+**MOGA controller support for Windows Phone 8.1/Windows 10 (UWP)**
+
+This simple and lightweight library will add "native" support of MOGA Bluetooth game controllers to your app.
+
+![](http://senssoft.com/img/MOGA.jpg)
+
+Library was tested with good and inexpensive [MOGA Mobile controller](https://www.amazon.com/gp/product/B0096L2SJ0/?tag=wpcentral00-20&ascsubtag=UUwpUvbUpU3563135); it also should work with the other MOGA game controllers.
+
+You can use Windows 8.1/WP 8.1 builds with your W8.1 UWP or W10 UWP apps and games.
+
+You may check this code on my [DOSBox app](https://www.microsoft.com/en-us/store/p/dosbox/9nblggh4q6j8) and [D00M game](https://www.microsoft.com/en-us/store/p/d00М/9nblggh4rql0)
+
+Enjoy!
---
+++ Shared/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using MOGAController;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace JoystickTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        Controller controller = new Controller();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            controller.StateChanged += Controller_StateChanged;
+            controller.KeyChanged += Controller_KeyChanged;
+            controller.AxisChanged += Controller_AxisChanged;
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (sender as Button);
+            if (btn.Content.Equals("Disconnect"))
+            {
+                controller.Disconnect();
+                keyStatus.Text = axisStatus.Text = "";
+                connectBDA.Content = "Connect to BD&A";
+                connectMoga2.Content = "Connect to Moga 2";
+                connectMoga2.IsEnabled = connectBDA.IsEnabled = true;
+            }
+            else
+            {
+                controller.Connect(btn.Content.Equals("Connect to BD&A") ? "BD&A" : "Moga 2");
+                (sender as Button).Content = "Disconnect";
+                if (btn.Name.Equals("connectBDA")) connectMoga2.IsEnabled = false; else connectBDA.IsEnabled = false;
+            }
+        }
+
+        private void Controller_StateChanged(StateEvent param)
+        {
+            controllerStatus.Text = param.StateValue.ToString();
+        }
+
+        private void Controller_KeyChanged(KeyEvent param)
+        {
+            keyStatus.Text = "keycode: " + param.KeyCode.ToString() + "   action: " + param.Action.ToString();
+        }
+
+        private void Controller_AxisChanged(MotionEvent param)
+        {
+            axisStatus.Text = "axis: " + param.Axis.ToString() + "   value: " + param.AxisValue.ToString();
+        }
+    }
+}
